d109daa1b563d167665a745195113f45a8df6062
Nursultan Kabylkas <nursultan.kabylkas@esperantotech.com>
2019-11-07 06:39:37 -0800

    created a script to add new posts

---
+++ new-post.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+name = sys.argv[1]
+
+os.system("mkdir -p ./raw/{}".format(name))
+os.system("echo '{0}' > ./raw/{0}/{0}.long".format(name))
+os.system("echo '{0}' > ./raw/{0}/{0}.short".format(name))
+os.system("echo '{0}' > ./raw/{0}/{0}.title".format(name))
+os.system("echo '{0}' > ./raw/{0}/{0}.meta".format(name))
